cf4f6f85e51fe84cd8b7db1005532ac7c4144f97
wilson azer <wilsonazer@hotmail.com>
2020-02-18 08:19:27 -0300

    update likeController

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const devController = require('./src/controllers/DevController')
+const likeController = require('./src/controllers/likeController')
 
 const router = express.Router()
 
 router.post('/post', devController.store )
-
+router.post('/devs/:dev_id/likes', likeController.store )
 
 module.exports = router
--- backend/src/controllers/likeController.js
+++ backend/src/controllers/likeController.js
@@ -2,6 +2,17 @@ const Dev = require('../models/DevModel')
 
 module.exports = {
     async store( req, res ){
-        
+        const  { user } = req.headers
+        const { dev_id } =  req.params
+
+        const loggedDev = await Dev.findById( user )
+        const targetDev = await Dev.findById( dev_id )
+
+        if( !targetDev ){
+            return res.status(400).json({ erro :'dev not exist'})
+        }
+
+       return res.json({  targetDev })
+
     }
 }
