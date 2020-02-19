c5d345dd744a5967328bab2a5ae16fc238f9f5f6
wilson azer <wilsonazer@hotmail.com>
2020-02-19 10:35:38 -0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express  = require('express')
 const router  = require('../routes')
 const mongoose = require('mongoose')
+const cors =  require('cors')
 
 const app  = express()
 //connectando com mongodb usando atlas server
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://wilson:51241314@cluster0-noxyc.mongodb.net/tinde
     useUnifiedTopology: true
 })
 
+app.use( cors() )
 app.use( express.json() )
 app.use( router )
 
