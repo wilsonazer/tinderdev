3991c3952b7baa7f354eacde1ecff4a47ff1167d
wilson azer <wilsonazer@hotmail.com>
2020-02-20 09:21:39 -0300

    add styles to login component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './Login.css'
 
 const Login = () =>(
-    <div>
-        <h1 className='logo'>Login</h1>
+    <div className='form-login'>
+        <h1 className='logo'>TindDev</h1>
         <form>
             <input type='text' name='login' placeholder='Informe seu login'/>
             <button type='submit' name='login'>Login</button>
